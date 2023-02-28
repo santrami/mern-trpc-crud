fadce305fde9fddd13073aa65b4e8a0b45b17a2c
santrami <santrami@hotmail.com>
2023-03-01 00:57:06 +0100

    creating vite client

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,16 @@ import express from "express";
 import morgan from "morgan";
 import * as trpcExpress from "@trpc/server/adapters/express";
 import { router, createContext } from "./trpc";
+import { notesRouter } from "./routes/notes";
 const app = express();
 
 app.use(morgan("dev"));
 
-const appRouter = router({});
+const appRouter = router({
+    note: notesRouter
+});
+
+
 
 app.use(
   "/trcp",
