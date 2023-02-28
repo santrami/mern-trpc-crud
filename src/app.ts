9d185138e3ba20c15d1157eb3733cfb298f0d244
santrami <santrami@hotmail.com>
2023-02-28 22:27:16 +0100

    trpc setup

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,19 @@
 import express from "express";
 import morgan from "morgan";
+import * as trpcExpress from "@trpc/server/adapters/express";
+import { router, createContext } from "./trpc";
 const app = express();
 
 app.use(morgan("dev"));
+
+const appRouter = router({});
+
+app.use(
+  "/trcp",
+  trpcExpress.createExpressMiddleware({
+    router: appRouter,
+    createContext,
+  })
+);
 
 export default app;
